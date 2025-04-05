1a382413ad581be61a33bcf748243b7ad55d1fb7
ksdev28 <kelvencarvalho28@gmail.com>
2025-04-05 03:20:44 -0300

    update

---
+++ README.md
@@ -0,0 +1,9 @@
+## Configuração do Ambiente
+
+    Para configurar o ambiente de desenvolvimento, basta executar o script `setup.sh`:
+
+    Para tornar o arquivo executável execute o comando:
+    chmod +x setup.sh
+
+bash
+  ./setup.sh
---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Clonar o repositório
+git clone https://github.com/ksdev28/Agendamentos-kivy.git
+cd agendamentos-kivy
+
+# Criar o ambiente Conda
+conda env create --file environment.yml
+
+# Ativar o ambiente
+# Isso pode variar dependendo do sistema operacional e do shell usado
+# No Linux/MacOS, você pode usar o comando:
+source $(conda info --base)/etc/profile.d/conda.sh
+conda activate nome_do_seu_ambiente
+
+# Instalar dependências adicionais, se necessário
+pip install kivy kivymd
+
+echo "Ambiente configurado e ativado. Pronto para uso!"
