3f07b39c8cf7a1090e38187d0e56038aaab96b82
edimundo silva <edimundobraga@gmail.com>
2025-04-06 12:43:55 -0300

    up main

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from kivymd.app import MDApp
 from telas import SistemaAgendamento
 
+
 class AgendamentoApp(MDApp):
     def build(self):
         return SistemaAgendamento()
+
 
 if __name__ == '__main__':
     AgendamentoApp().run()
